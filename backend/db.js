1e2b860460f47fdb692b18966b9c7ab1dd55878c
Saddam Alomari <saddamalomari99@gmail.com>
2025-12-22 16:07:04 +0300

    إعداد قاعدة البيانات السحابية وتجهيز الملفات للرفع

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,15 @@
-const mysql = require('mysql2/promise');
 require('dotenv').config();
+const mysql = require('mysql2');
 
-const db = mysql.createPool({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-module.exports = db;
+module.exports = pool.promise();
